90d32c8193f2eaf1f5fd7649196680e6572cf672
howardg0 <71763305+howardg0@users.noreply.github.com>
2026-08-11 21:04:22 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "block-log-v2.0";
+const CACHE = "block-log-v2.1";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
